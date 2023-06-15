21c04bf3c94e35eb770009215f4e595827328a57
katerinavr <katerina_vriza@yahoo.gr>
2023-06-15 17:02:11 -0500

    improving qa

--- params.py
+++ params.py
@@ -1,5 +1,5 @@
 #Load model and tokenizer
-model_name = "eachadea/vicuna-13b-1.1"
+model_name = "bert-base-uncased" #"eachadea/vicuna-13b-1.1"
 tokenizer_path = "./tokenizer/"
 
 chunk_size = 384 #Size of chunks to break the text store into
@@ -7,7 +7,7 @@ chunk_overlap = 32 #How much overlap between chunks
 seq_length = 2048 #LLM sequence length (Vicuna max is 2048)
 
 #Which embedding model to use?
-embedding_model_name = "all-mpnet-base-v2" #Highest scoring all-round, does 2800 sentences/s
+embedding_model_name =   "all-mpnet-base-v2" #Highest scoring all-round, does 2800 sentences/s
 #embedding_model_name = "all-MiniLM-L6-v2" #93% of best score, 5X faster
 
 init_docs = True #Recompute embeddings?
