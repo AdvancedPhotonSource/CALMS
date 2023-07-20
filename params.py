0a20c4b29e3dc2abc7a263ad4476b630103eba43
mcherukara <mathew.j.cherukara@gmail.com>
2023-07-20 16:59:34 -0500

    minor reorg of params file

--- params.py
+++ params.py
@@ -2,19 +2,18 @@
 set_visible_devices = True
 visible_devices = '2,3'
 
-#Load model and tokenizer
+#LLM parameters
 #model_name = "eachadea/vicuna-13b-1.1"
 model_name = "lmsys/vicuna-13b-v1.3"
-#model_name = "lmsys/vicuna-33b-v1.3"
 tokenizer_path = "./tokenizer/"
+seq_length = 2048 #LLM sequence length (Llama 2 max is 4k, Vicuna : 2048)
 
-chunk_size = 384 #Size of chunks to break the text store into
-chunk_overlap = 64 #How much overlap between chunks
-seq_length = 2048 #LLM sequence length (Vicuna max is 2048)
 
-#Which embedding model to use?
+#Embedding model parameters
 embedding_model_name =   "all-mpnet-base-v2" #Highest scoring all-round, does 2800 sentences/s
 #embedding_model_name = "all-MiniLM-L6-v2" #93% of best score, 5X faster
+chunk_size = 384 #Size of chunks to break the text store into
+chunk_overlap = 64 #How much overlap between chunks
 
 init_docs = False #Recompute embeddings?
 overwrite_embeddings = True #Overwrite embeddings if already exist? -- will raise val error of init_docs is True and this is not
