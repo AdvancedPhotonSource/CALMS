2a996b97519326c4cba2786a74e1d8d4f783a69d
katerinavr <katerina_vriza@yahoo.gr>
2023-09-15 11:51:41 -0500

    adding CNM-docs in doc_paths

--- params.py
+++ params.py
@@ -39,6 +39,7 @@ similarity_cutoff = 1.4 #Ignore context hits greater than this distance away. Em
 doc_paths = ["DOC_STORE/APS-Science-Highlight", 
              "DOC_STORE/APS-Docs", 
              "DOC_STORE/ALCF-Docs",
+             "DOC_STORE/CNM-Docs",
              "DOC_STORE/CNM-Science-Highlight"]
 
 
